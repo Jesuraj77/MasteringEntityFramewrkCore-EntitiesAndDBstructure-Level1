d1e4cc1c6c3750f8cbb9d0b48a8f8d097b9e54c2
itdev-lenovo <it1devops@gmail.com>
2020-03-27 16:59:10 +0530

    files added

---
+++ BusinessLayer/Services/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Services
+{
+   public  class Class1
+    {
+       // Create tablesin the ASP.Net application and map classes to the tables.Change the table names and move tables between its schemas.
+
+
+
+
+
+
+    }
+}
